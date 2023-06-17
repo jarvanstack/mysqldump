4f5e22d6474d7716db6c808a7419c7d4d372010f
dengjiayue <3508225629@qq.com>
2023-06-18 00:47:50 +0800

    GetDBName

--- util.go
+++ util.go
@@ -5,6 +5,9 @@ import (
 	"strings"
 )
 
+//从dns中提取出数据库名称，并将其作为结果返回。
+//如果无法解析出数据库名称，将返回一个错误。
+
 func GetDBNameFromDNS(dns string) (string, error) {
 	ss1 := strings.Split(dns, "/")
 	if len(ss1) == 2 {
